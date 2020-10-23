1aecf557c4687db8b1b445f25f3e9b3e3b6480ca
Anuj Gupta <angupta@itron.com>
2020-10-23 14:02:12 +0530

    Added Leave types Repositories, Added datatbalr, fontawesome icons

---
+++ LeaveManagement/Controllers/LeaveTypesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LeaveManagement.Contracts;
+using LeaveManagement.Data;
+using LeaveManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagement.Controllers
+{
+    public class LeaveTypesController : Controller
+    {
+        private readonly ILeaveTypeRepository _leaveRepo;
+        private readonly IMapper _mapper;
+
+        public LeaveTypesController(ILeaveTypeRepository leaveRepo, IMapper mapper)
+        {
+            _leaveRepo = leaveRepo;
+            _mapper = mapper;
+        }
+
+        // GET: LeaveTypesController
+        public ActionResult Index()
+        {
+            var leaveTypes = _leaveRepo.FindAll().ToList();
+            var model = _mapper.Map<List<LeaveType>, List<LeaveTypeVM>>(leaveTypes);
+            return View(model);
+        }
+
+        // GET: LeaveTypesController/Details/5
+        public ActionResult Details(int id)
+        {
+            var leaveType = _leaveRepo.FindById(id);
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+            var model = _mapper.Map<LeaveTypeVM>(leaveType);
+            return View(model);
+        }
+
+        // GET: LeaveTypesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(LeaveTypeVM model)
+        {
+            try
+            {
+                if(!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var leaveType = _mapper.Map<LeaveType>(model);
+                leaveType.DateCreated = DateTime.Now;
+
+                var isSuccess = _leaveRepo.Create(leaveType);
+                if (!isSuccess)
+                {
+                    ModelState.AddModelError("", "Something Went Wrong...");
+                    return View(model);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Something Went Wrong...");
+                return View(model);
+            }
+        }
+
+        // GET: LeaveTypesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var leaveType = _leaveRepo.FindById(id);
+            if(leaveType == null)
+            {
+                return NotFound();
+            }
+            var model = _mapper.Map<LeaveTypeVM>(leaveType);
+            return View(model);
+        }
+
+        // POST: LeaveTypesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(LeaveTypeVM model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                var leaveType = _mapper.Map<LeaveType>(model);
+                var isSuccess = _leaveRepo.Update(leaveType);
+                if(!isSuccess)
+                {
+                    ModelState.AddModelError("", "Something Went Wrong...");
+                    return View(model);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Something Went Wrong...");
+                return View(model);
+            }
+        }
+
+        // GET: LeaveTypesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var leaveType = _leaveRepo.FindById(id);
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+            var isSuccess = _leaveRepo.Delete(leaveType);
+            if (!isSuccess)
+            {
+                return BadRequest();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: LeaveTypesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, LeaveTypeVM model)
+        {
+            try
+            {
+                var leaveType = _leaveRepo.FindById(id);
+                if(leaveType == null)
+                {
+                    return NotFound();
+                }
+                var isSuccess = _leaveRepo.Delete(leaveType);
+                if (!isSuccess)
+                {                    
+                    return View(model);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+    }
+}
--- LeaveManagement/Data/ApplicationDbContext.cs
+++ LeaveManagement/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using LeaveManagement.Models;
 
 namespace LeaveManagement.Data
 {
@@ -13,9 +14,10 @@ namespace LeaveManagement.Data
         {
         }
 
-        DbSet<Employee> Employees { get; set; }
-        DbSet<LeaveType> LeaveTypes { get; set; }
-        DbSet<LeaveAllocation> leaveAllocations { get; set; }
-        DbSet<LeaveHistory> LeaveHistories { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> leaveAllocations { get; set; }
+        public DbSet<LeaveHistory> LeaveHistories { get; set; }
+        public DbSet<LeaveManagement.Models.LeaveTypeVM> DetailsLeaveTypeVM { get; set; }
     }
 }
